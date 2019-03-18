7ddf258c5edf45a474702c22497cbedff932c0a0
savinabeysooriya <savinabeysooriya@gmail.com>
2019-03-19 02:06:09 +0530

    login and registration 1.0

--- app/src/main/java/com/savin/uberapp/MainActivity.java
+++ app/src/main/java/com/savin/uberapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.savin.uberapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,7 +21,11 @@ public class MainActivity extends AppCompatActivity {
         mDriver.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
+                Intent intent = new Intent(MainActivity.this, DriverLoginActivity.class);
+                startActivity(intent);
+                finish();
+                return;
+
             }
         });
 
