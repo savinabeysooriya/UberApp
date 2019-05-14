f2c59f0e622eb693c0531fd62659ddf4173987dc
savinabeysooriya <savinabeysooriya@gmail.com>
2019-05-15 02:31:05 +0530

    Google map API

--- app/src/main/java/com/savin/uberapp/DriverLoginActivity.java
+++ app/src/main/java/com/savin/uberapp/DriverLoginActivity.java
@@ -35,7 +35,7 @@ public class DriverLoginActivity extends AppCompatActivity {
 
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if(user != null){
-                    Intent intent = new Intent(DriverLoginActivity.this, MapActivity.class);
+                    Intent intent = new Intent(DriverLoginActivity.this, DriverMapActivity.class);
                     startActivity(intent);
                     finish();
                     return;
