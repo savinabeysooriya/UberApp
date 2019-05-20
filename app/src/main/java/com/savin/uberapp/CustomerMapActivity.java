8c78cbd67b1a03b14db1a8d181f43a8ba88d9a7c
savinabeysooriya <savinabeysooriya@gmail.com>
2019-05-20 23:01:08 +0530

    get closest driver and which driver availble

--- app/src/main/java/com/savin/uberapp/CustomerMapActivity.java
+++ app/src/main/java/com/savin/uberapp/CustomerMapActivity.java
@@ -27,12 +27,17 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class CustomerMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
@@ -105,7 +110,16 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
             public void onDataEntered(DataSnapshot dataSnapshot, GeoLocation location) {
                if(!driverFound){
                    driverFound = true;
-                   driverFoundID =dataSnapshot.toString() ;
+                   driverFoundID =dataSnapshot.toString() ; //who is the working for customer
+
+                   DatabaseReference DriverRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(driverFoundID);
+                   String customerId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                   HashMap map = new HashMap();
+                   map.put("customerRideId",customerId);
+                   DriverRef.updateChildren(map);  //customer id will be inside in driver child in database
+                                                        // tell the driver which customer that attend to driver
+                   getDriverLocation();
+                   mRequest.setText("Looking for Driver...");
                }
 
             }
@@ -138,6 +152,39 @@ public class CustomerMapActivity extends FragmentActivity implements OnMapReadyC
 
             }
         });
+    }
+    private Marker mDriverMarker;
+    private void getDriverLocation(){
+        DatabaseReference DriverLocationRef = FirebaseDatabase.getInstance().getReference().child("driversWorking").child("driverFoundID").child("l");
+        DriverLocationRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                     List<Object> map = (List<Object>)dataSnapshot.getValue();
+                     double locationLat = 0;
+                     double locationLng = 0;
+                    mRequest.setText("Driver Found");
+
+                    if(map.get(0) != null){
+                        locationLat = Double.parseDouble(map.get(0).toString());
+                    }
+                    if(map.get(1) != null){
+                        locationLng = Double.parseDouble(map.get(1).toString());
+                    }
+                    LatLng driverLatLng = new LatLng(locationLat,locationLng);
+                    if(mDriverMarker != null){
+                        mDriverMarker.remove();         //Tell the customer where his driver location
+                    }
+                    mDriverMarker = mMap.addMarker(new MarkerOptions().position(driverLatLng).title("your driver"));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
--- app/src/main/java/com/savin/uberapp/DriverMapActivity.java
+++ app/src/main/java/com/savin/uberapp/DriverMapActivity.java
@@ -27,9 +27,14 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+import java.util.Map;
 
 public class DriverMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener {
 
@@ -39,6 +44,9 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
     LocationRequest mLocationRequest;
 
     private Button mLogout;
+
+    private String customerId = "";
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,8 +67,60 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
                 return;
             }
         });
+
+        getAssignedCustomer();
+    }
+    private void getAssignedCustomer(){
+        String driverId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference assignedcustomerRef = FirebaseDatabase.getInstance().getReference().child("Users").child("Drivers").child(driverId);
+        assignedcustomerRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    Map<String, Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                    if(map.get("customerRideId") != null){
+                        customerId = map.get("customerRideId").toString(); //pickup location from customer id
+                        getAssignedCustomerPickupLocation();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
+    private void getAssignedCustomerPickupLocation(){
+        DatabaseReference assignedCustomerPickupLocationRef = FirebaseDatabase.getInstance().getReference().child("customerRequest").child(customerId).child("l");
+        assignedCustomerPickupLocationRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                   List<Object> map = (List<Object>) dataSnapshot.getValue();
+                    double locationLat = 0;
+                    double locationLng = 0;
+
+
+                    if(map.get(0) != null){
+                        locationLat = Double.parseDouble(map.get(0).toString());
+                    }
+                    if(map.get(1) != null){
+                        locationLng = Double.parseDouble(map.get(1).toString());
+                    }
+                    LatLng driverLatLng = new LatLng(locationLat,locationLng);
+
+                    mMap.addMarker(new MarkerOptions().position(driverLatLng).title("pickup location"));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -114,24 +174,53 @@ public class DriverMapActivity extends FragmentActivity implements OnMapReadyCal
 
     @Override
     public void onLocationChanged(Location location) {
-       mLastLocation = location;
+     if(getApplicationContext() != null) {
 
-       LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+         mLastLocation = location;
+         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+         mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
 
-       mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-       mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+         String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+         DatabaseReference refAvailable = FirebaseDatabase.getInstance().getReference("driverAvailable");
+         DatabaseReference refWorking = FirebaseDatabase.getInstance().getReference("driverWorking");
+         GeoFire geoFireAvailable = new GeoFire(refAvailable);
+         GeoFire geoFireWorking = new GeoFire(refWorking);
 
-       String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("driverAvailable");
+         switch (customerId){
+             case "":
 
-        GeoFire geoFire = new GeoFire(ref);
-        geoFire.setLocation(userId, new GeoLocation(location.getLatitude(),location.getLongitude()), new GeoFire.CompletionListener() {
+                 geoFireWorking.removeLocation(userId);
+                 geoFireAvailable.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()), new GeoFire.CompletionListener() {
 
-            @Override
-            public void onComplete(String key, DatabaseError error) {
+                     @Override
+                     public void onComplete(String key, DatabaseError error) {
 
-            }
-        });
+                     }
+                 });
+
+                 break;
+
+             default:
+
+                 geoFireWorking.removeLocation(userId);
+                 geoFireWorking.setLocation(userId, new GeoLocation(location.getLatitude(), location.getLongitude()), new GeoFire.CompletionListener() {
+
+                     @Override
+                     public void onComplete(String key, DatabaseError error) {
+
+                     }
+                 });
+                 break;
+         }
+
+
+
+
+
+
+
+     }
     }
 
     @Override
